afc44163387f3c29a9c22bfd2e145a3b335b18d0
David Santander <david.santander.19@gmail.com>
2019-12-17 21:47:03 -0600

    Started table creation for the consumer complaints db

--- Data/sql-steps.sql
+++ Data/sql-steps.sql
@@ -6,5 +6,23 @@ CREATE USER 'finance_admin'@'localhost' IDENTIFIED BY 'finance_bedu'
 GRANT ALL PRIVILEGES ON financedb.* TO 'finance_admin'@'localhost';
 /* Display MySQL user account privileges */
 SHOW GRANTS FOR 'finance_admin'@'localhost';
-
-
+/* Create the table for the consumer complaints */
+CREATE TABLE user_complaints(
+    date_received   DATE,
+    product VARCHAR(),
+    sub_product VARCHAR(),
+    issue VARCHAR(),
+    sub_issue VARCHAR(),
+    complaint_narrative VARCHAR(),
+    company_public_response VARCHAR(),
+    state VARCHAR(),
+    zip_code VARCHAR(),
+    tags VARCHAR(),
+    consent_provided VARCHAR(),
+    submitted_via VARCHAR(),
+    date_sent_company DATE,
+    company_consumer_response VARCHAR(),
+    timely_response BOOLEAN,
+    consumer_disrupted VARCHAR(),
+    complaint_id 
+);
