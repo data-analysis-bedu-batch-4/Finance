a11ce3f5a7842c25333fbc520119de4ea2059ad0
David Santander <david.santander.19@gmail.com>
2019-12-18 20:46:26 -0600

    Add the csv file to sql table without success

--- Data/sql-steps.sql
+++ Data/sql-steps.sql
@@ -7,22 +7,50 @@ GRANT ALL PRIVILEGES ON financedb.* TO 'finance_admin'@'localhost';
 /* Display MySQL user account privileges */
 SHOW GRANTS FOR 'finance_admin'@'localhost';
 /* Create the table for the consumer complaints */
-CREATE TABLE user_complaints(
-    date_received   DATE,
-    product VARCHAR(),
-    sub_product VARCHAR(),
-    issue VARCHAR(),
-    sub_issue VARCHAR(),
-    complaint_narrative VARCHAR(),
-    company_public_response VARCHAR(),
-    state VARCHAR(),
-    zip_code VARCHAR(),
-    tags VARCHAR(),
-    consent_provided VARCHAR(),
-    submitted_via VARCHAR(),
-    date_sent_company DATE,
-    company_consumer_response VARCHAR(),
-    timely_response BOOLEAN,
-    consumer_disrupted VARCHAR(),
-    complaint_id 
+CREATE TABLE consumer_complaints(
+    date_received VARCHAR(15),
+    product VARCHAR(50),
+    sub_product VARCHAR(100),
+    issue VARCHAR(100),
+    sub_issue VARCHAR(100),
+    consumer_complaint_narrative TEXT,
+    company_public_response TEXT,
+    company VARCHAR(40),
+    state VARCHAR(3),
+    zip_code VARCHAR(10),
+    tags VARCHAR(30),
+    consent_provided VARCHAR(30),
+    submitted_via VARCHAR(20),
+    date_sent_company VARCHAR(15),
+    company_response_consumer VARCHAR(50),
+    timely_response VARCHAR(10),
+    consumer_disrupted VARCHAR(10),
+    complaint_id INT,
+    PRIMARY KEY(complaint_id)
+);
+/* Add the csv file data to the db table */
+LOAD DATA LOCAL INFILE 'Consumer_Complaints.csv' 
+INTO TABLE consumer_complaints
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+ (
+    date_received,
+    product,
+    sub_product,
+    issue,
+    sub_issue,
+    @dummy,
+    company_public_response,
+    company,
+    state,
+    zip_code,
+    tags,
+    consent_provided,
+    submitted_via,
+    date_sent_company,
+    company_response_consumer,
+    timely_response,
+    consumer_disrupted,
+    complaint_id
 );
