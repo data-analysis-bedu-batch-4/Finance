ade1d7f9b38f6e7dacab5367fab65c897ee11f93
DavidSantander <santander.david.19@gmail.com>
2020-04-10 15:15:55 -0500

    Add docker-compose and config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { WorkerModule } from './worker/worker.module';
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { WorkerModule } from "./worker/worker.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import databaseConfig from "./config/database.config";
 @Module({
-  imports: [WorkerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".development.env",
+      isGlobal: true,
+      load: [databaseConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres" as "postgres",
+        host: configService.get<string>("database.host"),
+        port: configService.get<number>("database.port"),
+        username: configService.get<string>("database.user"),
+        password: configService.get<string>("database.password"),
+        database: configService.get<string>("database.dbName"),
+        entities: [__dirname + "/entity/*.entity{.ts,.js}"]
+        // synchronize: true
+      }),
+      inject: [ConfigService]
+    }),
+    WorkerModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
