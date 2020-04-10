c1629dae9ae5f23cc31d3693d23ba533f9b9044d
DavidSantander <santander.david.19@gmail.com>
2020-04-09 22:42:41 -0500

    Add worker servie & module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
-  imports: [],
+  imports: [WorkerModule],
   controllers: [AppController],
   providers: [AppService],
 })
