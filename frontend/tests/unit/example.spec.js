e10b1a70c3de42331396e3303d57b065218fec94
DSanta07 <jsantander@nxtiot.io>
2020-04-11 19:40:09 -0500

    Change test config

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -2,6 +2,4 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 test('App should work', () => {
-  const wrapper = shallowMount(App)
-  expect(wrapper.text()).toMatch('Welcome to Your Vue.js App')
 })
