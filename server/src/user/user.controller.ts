305989f2bf851f2c324bb930b0ea4ce901f3b320
DSanta07 <jsantander@nxtiot.io>
2020-04-14 01:05:33 -0500

    nginx change

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,14 +3,14 @@ import { UserInsertionDto } from './user-insertion.dto';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
-@Controller('/user')
+@Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
-    @Post("/insert")
+    @Post("insert")
     async insertUser(@Body() userInsertionDto: UserInsertionDto): Promise<User> {
         return await this.userService.insertUser(userInsertionDto);
     }
-    @Get("/all")
+    @Get("all")
     async getAllUsers(): Promise<User[]> {
         return await this.userService.getAllUsers();
     }
